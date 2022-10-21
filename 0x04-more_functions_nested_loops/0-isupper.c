a42a0b77b65f832390d608d410993c03d445f83d
Roland-Odez <breezyroland@gmail.com>
2022-10-20 20:26:52 -0700

    checks for uppercase letter

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase letter
+ * @c: character to check
+ */
+
+int _isupper(char c)
+{
+	int i = 'A';
+	
+	while(i < 'Z')
+	{
+		if(i == c){
+		       return (1);
+		}
+ 	}
+
+	return (0);
+}	
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+
+/**
+ * _putchar - puts charatar into stdio
+ */
+int _putchar(char c);
+
+/**
+ * _isupper - checks uppercae letter
+ * @c : character to check
+ * Retrun: 1 for success, 0 for fail
+ */
+
+int _isupper(char c);
+#endif
