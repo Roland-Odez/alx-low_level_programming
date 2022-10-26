2e50a16323a73844882293ec32f86c52d7e7cb7a
Roland-Odez <breezyroland@gmail.com>
2022-10-25 21:22:29 -0700

    asigns a pointer parameter to value 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - assigns a pointer parameter to value of 98
+ * @n: pointer to int n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _Putchar - puts character to standard output
+ * @c: character to put
+ */
+
+in _putchar(char c);
+
+/**
+ * reset_to_98 - updates value of pointer to 98
+ * *@n: pointer to int n
