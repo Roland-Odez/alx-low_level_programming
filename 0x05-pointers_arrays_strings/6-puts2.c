cb6aa681cb14eef8c4b27b08e067f890e314a526
Roland-Odez <breezyroland@gmail.com>
2022-10-26 00:16:08 -0700

    print every second character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ *puts2 - prints evry second term of a character string
+ * @str: string
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
