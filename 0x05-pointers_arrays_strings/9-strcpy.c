7f3adb80595b9a524fd0437f66e3c6e6010443da
Roland-Odez <breezyroland@gmail.com>
2022-10-26 06:12:45 -0700

    function to copy string from pointer to pointer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strlen - return length of string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+
+}
+
+/**
+ * _strcpy - copies the string from pointed to src to string pointed to dest, including terminating null
+ * @dest:source to copy to
+ * @src: source to copy from
+ * Return: copy of original source
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	for (i = =; i <= _strlen(src); i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
