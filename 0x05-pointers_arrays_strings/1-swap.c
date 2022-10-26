120f5f62334ad179857add161022154b0ba6f978
Roland-Odez <breezyroland@gmail.com>
2022-10-25 21:35:32 -0700

    swap integer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * awap_int - swaps two integers
+ * @a: first integer to swap
+ * @b: second integer to swap
+ */
+
+void swap_int(int *a,int *b)
+{
+	int temp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
