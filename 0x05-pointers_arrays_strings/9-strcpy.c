dc6e10e71a9b3d64cddc120ee0d609fe276bab46
Roland-Odez <breezyroland@gmail.com>
2022-10-26 06:14:57 -0700

    assign variable i to 0

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -29,7 +29,7 @@ int _strlen(char *s)
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-	for (i = =; i <= _strlen(src); i++)
+	for (i = 0; i <= _strlen(src); i++)
 		dest[i] = src[i];
 
 	return (dest);
