1d2c76487805f59a9008608b3627f51434102420
Roland-Odez <breezyroland@gmail.com>
2022-10-26 00:50:49 -0700

    print half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s:string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+}
+
+/**
+ * puts_half - printshalf of a strnig, followed bt a new line
+ * @str: string to print from
+ */
+
+void puts_half(char *str)
+{
+	int idx;
+	int len = _strlen(str);
+
+	/* find the index to start depending on even/odd amount of strlen */
+	if(len % 2 != 0)
+		idx = (len / 2) + 1;
+	else
+		idx = (len / 2);
+
+	while (idx < len)
+	{
+		_putchar(*(str + idx));
+		idx++;
+	}
+	_putchar('\n');
+}
