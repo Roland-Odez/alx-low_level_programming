732526c64c5d93bda172b15d38159b75ff937686
Roland-Odez <breezyroland@gmail.com>
2022-10-31 13:05:19 -0700

    func to copy n bytes of memory from source pointer to destination pointer

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,29 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - puts character to standard outpu
+ * @c: character to put to standard output
+ */
+
+int _putchar(char c);
+
+/**
+ * _memset - fill n bytes of memory with a constant value
+ * @s: pointer to memory area
+ * @b: constant value
+ * @n: number of bytes to fill
+ * Return: memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+
+/**
+ * _memcpy - copy n bytes of memory from source to destination
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n);
