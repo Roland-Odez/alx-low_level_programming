b58eba2a5c132f4c9d4851dcec23706aaac859c5
Roland-Odez <breezyroland@gmail.com>
2022-10-31 13:39:39 -0700

    return pointer to byte in s that matches a byte in accept

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -46,4 +46,36 @@ char *_strchr(char *s, char c);
 
 unsigned int _strspn(char *s, char *accept);
 
+/**
+ * _strstr - locate and return pointer to first occurence of substring
+ * @haystack: string to search
+ * @needle: target substring to search for
+ * Return: pointer to index of string at first occurence of whole substring
+ */
+
+char *_strstr(char *haystack, char *needle);
+
+/**
+ * print_chessboard - print chessboard given set 2D array
+ * @a: 2D array
+ */
+
+void print_chessboard(char (*a)[8]);
+
+/**
+ * print_diagsums - print sums of diagonals in matrix
+ * @a: matrix
+ * @size: size of matrix
+ */
+
+void print_diagsums(int *a, int size);
+
+/**
+ * set_string - set value of pointer to a cha
+ * @s: variable of type pointer
+ * @to: char
+ */
+
+void set_string(char **s, char *to);
+
 #endif
