aafdf993223b5d963d2caefa0419857cf1ca945e
Roland-Odez <breezyroland@gmail.com>
2022-10-31 15:13:39 -0700

    make proper spacing

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,11 +1,11 @@
 #include "main.h"
-#define NULL 0
+
 
 /**
  * _strstr - locate and return pointer to first occurence of substring
  * @haystack: string to search
  * @needle: target substring to search for
- * Return: pointer to index of string at first occurence of whole substring
+ * Return: 0 on success
  */
 
 char *_strstr(char *haystack, char *needle)
