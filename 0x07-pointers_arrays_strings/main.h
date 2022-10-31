8be1dce21356ed6db1baa0fcf7b1eb8e9dbbd91b
Roland-Odez <breezyroland@gmail.com>
2022-10-31 13:23:01 -0700

    locate 1st occurence of char in string

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -28,4 +28,22 @@ char *_memset(char *s, char b, unsigned int n);
 
 char *_memcpy(char *dest, char *src, unsigned int n);
 
+/**
+ * _strchr - locate 1st occurrence of character in string and returns pointer 
+ * @s: string to search
+ * @c: target character
+ * Return: pointer to that character in string
+ */
+
+char *_strchr(char *s, char c);
+
+/**
+* _strspn - return length of string that matches values consistently
+* @s: string to search
+* @accept: target matches
+* Return: number of bytes consecutively matched
+*/
+
+unsigned int _strspn(char *s, char *accept);
+
 #endif
